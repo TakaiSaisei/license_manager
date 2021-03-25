80b370bf9fccf707692ee896742ce0bd97dcb3a6
TakaiSaisei <takaisaisei@gmail.com>
2021-03-26 02:31:50 +0300

    Cleanup

--- app/services/flussonic_check_license.rb
+++ app/services/flussonic_check_license.rb
@@ -2,20 +2,18 @@ class FlussonicCheckLicense
   def check(license)
     last_version = Date.strptime(FlussonicLastVersion.new.get, '%y.%m')
     avail_licenses = []
-    version = last_version.prev_month
-    avail_licenses << version.strftime('%y.%m')
-    4.times do
+    version = last_version
+    5.times do
       version = version.prev_month
       avail_licenses << version.strftime('%y.%m')
     end
-
-    puts avail_licenses
 
     avail_licenses.delete_if do |date|
       (!license.max_version.nil? { date > license.max_version }) |
         (!license.min_version.nil? { date < license.min_version }) |
         (date < license.paid_till)
     end
+
     if avail_licenses.empty?
       last_version
     else
